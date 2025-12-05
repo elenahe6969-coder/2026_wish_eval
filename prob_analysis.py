fba4931a9465010d387658c22b5201ba5fb25bc9
elenahe6969-coder <elenahe6969@gmail.com>
2025-12-05 17:38:05 +0800

    Update prob_analysis.py

--- prob_analysis.py
+++ prob_analysis.py
@@ -199,9 +199,9 @@ if shared_wish_id and shared_wish_text:
         st.markdown("</div>", unsafe_allow_html=True)
     
     st.markdown("""
-    **Message from your friend:**
-    *"Merry Christmas! I just made a wish for 2026. 
-    Please click the heart button to share your Christmas luck and help make my wish come true!"*
+    Message from your friend:
+    "Merry Christmas! I just made a wish for 2026. 
+    Please click the heart button to share your Christmas luck and help make my wish come true!"
     """)
     st.markdown("---")
     
